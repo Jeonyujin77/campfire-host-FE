a2eced82eb8b1311edd8dcbd5262d5834883a1bf
Jeonyujin77 <wjsdbwls7777@gmail.com>
2023-02-03 13:28:03 +0900

    [STYLE] 공지 추가

--- src/routes/Signin.tsx
+++ src/routes/Signin.tsx
@@ -38,6 +38,29 @@ const Signin = () => {
   return (
     <SigninWrapper>
       <SigninHeader>Camp-Fire</SigninHeader>
+      <div style={{ lineHeight: 1.4 }}>
+        <p>📣 CampFire 호스트 서비스 이용 방법 📣</p>
+        <p>
+          <br />
+          현재 사업자 확인을 하지 않으면 회원가입이 불가능합니다. 따라서 호스트
+          서비스에서도 많은 분들의 피드백을 받기 위해 런칭 기간 동안 호스트
+          서비스를 이용하실 수 있도록 방법을 고안했습니다.
+        </p>
+        <p>
+          <br />
+          런칭 기간 동안 해당 호스트로 로그인 부탁드립니다. <br />
+          - 이메일 : hjs7590@naver.com <br />- 비밀번호 : 1234qwer
+        </p>
+        <p>
+          <br />
+          👍해당 호스트는 테스트용으로 자유롭게 기능들 이용해보실 수 있습니다.
+        </p>
+        <p>
+          다만 많은 유저들이 사용하는 공간이오니 부적절한 언어 및 표현은
+          삼가해주시기 바랍니다.
+        </p>
+        <br />
+      </div>
       <Paper style={{ padding: "20px 0" }}>
         <SigninForm onSubmit={onSubmit}>
           <FormGrp>
